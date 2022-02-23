5ed11759fe94e4ec52101ce241c90574827455a4
Adrien <garrousteadrien@gmail.com>
2022-02-23 21:21:27 +0100

    Update scope

--- chart.go
+++ chart.go
@@ -1,11 +1,5 @@
 package chart
 
-// type chart for struct who implement theses methods
-type chart interface {
-	New(title string, ticks float64, labels []string, values []string, fill string) *chart
-	Generate(chartType int) string
-}
-
 // barchart is just a barchart with
 // a title to describe it
 //  a tick unit (ex: 0.5)
